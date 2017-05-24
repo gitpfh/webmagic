aa993bfae8fe83a905885a1c9f7cd78cec283c01
gitpfh <pfh_hit@163.com>
2017-05-24 21:41:29 +0800

    2141

---
+++ src/main/java/org/eking/webmagic/GithubRepoPageProcessor.java
@@ -0,0 +1,37 @@
+
+package webmagictest;
+
+import java.util.List;
+
+import us.codecraft.webmagic.Page;
+import us.codecraft.webmagic.Site;
+import us.codecraft.webmagic.Spider;
+import us.codecraft.webmagic.pipeline.ConsolePipeline;
+import us.codecraft.webmagic.processor.PageProcessor;
+
+public class GithubRepoPageProcessor implements PageProcessor {
+
+	// 部分一：抓取网站的相关配置，包括编码、抓取间隔、重试次数等
+	private Site site = Site.me().setRetryTimes(3).setSleepTime(1000);
+
+	@Override
+	// process是定制爬虫逻辑的核心接口，在这里编写抽取逻辑
+	public void process(Page page) {
+		// 部分二：定义如何抽取页面信息，并保存下来
+		List<String> links = page.getHtml().links().regex("(http://www\\.baikemy\\.com/disease/list/1/34\\?pageIndex=\\w+\\&pageCount\\=9)").all();
+		page.addTargetRequests(links);
+		page.putField("title", page.getHtml().xpath("//div[@class='BlogEntity']/div[@class='BlogTitle']/h1").toString());
+		page.putField("content", page.getHtml().xpath("//li[@class='ccjb_jbli_li']/a/text()").all());
+		//page.putField("tags", page.getHtml().xpath("//div[@class='BlogTags']/a/text()").all());
+	}
+
+	@Override
+	public Site getSite() {
+		return site;
+
+	}
+
+	public static void main(String[] args) {
+		Spider.create(new GithubRepoPageProcessor()).addUrl("http://www.baikemy.com/disease/list/1/34?pageIndex=1&pageCount=9").addPipeline(new ConsolePipeline()).run();
+	}
+}
