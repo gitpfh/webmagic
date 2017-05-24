c21592ef31f9eb92f346e35f79419bd2a1851167
pfh <pfh_hit@163.com>
2017-05-24 18:37:54 +0800

    测试

---
+++ src/main/java/org/eking/webmagic/App.java
@@ -0,0 +1,13 @@
+package org.eking.webmagic;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
