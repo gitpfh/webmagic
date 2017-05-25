8e48527f206c2f48fd478c7dd6b32ba8f1b85993
gitpfh <pfh_hit@163.com>
2017-05-25 21:01:40 +0800

    change

--- src/main/java/org/eking/webmagic/GithubRepoPageProcessor.java
+++ src/main/java/org/eking/webmagic/GithubRepoPageProcessor.java
@@ -1,19 +1,28 @@
 
 package org.eking.webmagic;
 
+import java.util.List;
+
 import us.codecraft.webmagic.Page;
+import us.codecraft.webmagic.Request;
 import us.codecraft.webmagic.Site;
 import us.codecraft.webmagic.Spider;
 import us.codecraft.webmagic.processor.PageProcessor;
 
 public class GithubRepoPageProcessor implements PageProcessor {
 
-	private Site site = Site.me().setRetryTimes(3).setSleepTime(100);
+	private Site site = Site.me().setCycleRetryTimes(5).setSleepTime(100).setTimeOut(50000);
 
 	@Override
 	public void process(Page page) {
-		//List<String> links = page.getHtml().links().regex("(http://www\\.a\\-hospital\\.com/w/疾病条目索引\\-[a-zA-Z])").all();
-		//page.addTargetRequests(links);
+		List<String> links = page.getHtml().$("div#bodyContent a", "href").all();
+
+		for (String link : links) {
+			if (link.matches(".*//www\\.a-hospital\\.com/w/%E7%96%BE%E7%97%85%E6%9D%A1%E7%9B%AE%E7%B4%A2%E5%BC%95-[a-zA-Z]")) {
+				Request request = new us.codecraft.webmagic.Request(link);
+				page.addTargetRequest(request);
+			}
+		}
 		//page.putField("title", page.getHtml().xpath("//div[@class='BlogEntity']/div[@class='BlogTitle']/h1").toString());
 		page.putField("content", page.getHtml().xpath("//div[@id='bodyContent']/ul/li/a/text()").all());
 		//page.putField("tags", page.getHtml().xpath("//div[@class='BlogTags']/a/text()").all());
@@ -26,28 +35,6 @@ public class GithubRepoPageProcessor implements PageProcessor {
 	}
 
 	public static void main(String[] args) {
-		Spider.create(new GithubRepoPageProcessor()).addUrl("http://www.a-hospital.com/w/疾病条目索引-A").addPipeline(new FilePipeline()).run();
-		Spider.create(new GithubRepoPageProcessor()).addUrl("http://www.a-hospital.com/w/疾病条目索引-B").addPipeline(new FilePipeline()).run();
-		Spider.create(new GithubRepoPageProcessor()).addUrl("http://www.a-hospital.com/w/疾病条目索引-C").addPipeline(new FilePipeline()).run();
-		Spider.create(new GithubRepoPageProcessor()).addUrl("http://www.a-hospital.com/w/疾病条目索引-D").addPipeline(new FilePipeline()).run();
-		Spider.create(new GithubRepoPageProcessor()).addUrl("http://www.a-hospital.com/w/疾病条目索引-E").addPipeline(new FilePipeline()).run();
-		Spider.create(new GithubRepoPageProcessor()).addUrl("http://www.a-hospital.com/w/疾病条目索引-F").addPipeline(new FilePipeline()).run();
-		Spider.create(new GithubRepoPageProcessor()).addUrl("http://www.a-hospital.com/w/疾病条目索引-G").addPipeline(new FilePipeline()).run();
-		Spider.create(new GithubRepoPageProcessor()).addUrl("http://www.a-hospital.com/w/疾病条目索引-H").addPipeline(new FilePipeline()).run();
-		Spider.create(new GithubRepoPageProcessor()).addUrl("http://www.a-hospital.com/w/疾病条目索引-J").addPipeline(new FilePipeline()).run();
-		Spider.create(new GithubRepoPageProcessor()).addUrl("http://www.a-hospital.com/w/疾病条目索引-K").addPipeline(new FilePipeline()).run();
-		Spider.create(new GithubRepoPageProcessor()).addUrl("http://www.a-hospital.com/w/疾病条目索引-L").addPipeline(new FilePipeline()).run();
-		Spider.create(new GithubRepoPageProcessor()).addUrl("http://www.a-hospital.com/w/疾病条目索引-M").addPipeline(new FilePipeline()).run();
-		Spider.create(new GithubRepoPageProcessor()).addUrl("http://www.a-hospital.com/w/疾病条目索引-N").addPipeline(new FilePipeline()).run();
-		Spider.create(new GithubRepoPageProcessor()).addUrl("http://www.a-hospital.com/w/疾病条目索引-O").addPipeline(new FilePipeline()).run();
-		Spider.create(new GithubRepoPageProcessor()).addUrl("http://www.a-hospital.com/w/疾病条目索引-P").addPipeline(new FilePipeline()).run();
-		Spider.create(new GithubRepoPageProcessor()).addUrl("http://www.a-hospital.com/w/疾病条目索引-Q").addPipeline(new FilePipeline()).run();
-		Spider.create(new GithubRepoPageProcessor()).addUrl("http://www.a-hospital.com/w/疾病条目索引-R").addPipeline(new FilePipeline()).run();
-		Spider.create(new GithubRepoPageProcessor()).addUrl("http://www.a-hospital.com/w/疾病条目索引-S").addPipeline(new FilePipeline()).run();
-		Spider.create(new GithubRepoPageProcessor()).addUrl("http://www.a-hospital.com/w/疾病条目索引-T").addPipeline(new FilePipeline()).run();
-		Spider.create(new GithubRepoPageProcessor()).addUrl("http://www.a-hospital.com/w/疾病条目索引-W").addPipeline(new FilePipeline()).run();
-		Spider.create(new GithubRepoPageProcessor()).addUrl("http://www.a-hospital.com/w/疾病条目索引-X").addPipeline(new FilePipeline()).run();
-		Spider.create(new GithubRepoPageProcessor()).addUrl("http://www.a-hospital.com/w/疾病条目索引-Y").addPipeline(new FilePipeline()).run();
-		Spider.create(new GithubRepoPageProcessor()).addUrl("http://www.a-hospital.com/w/疾病条目索引-Z").addPipeline(new FilePipeline()).run();
+		Spider.create(new GithubRepoPageProcessor()).addUrl("http://baike.baidu.com/item/皮肤滤泡中心淋巴瘤").addPipeline(new FilePipeline()).thread(10).run();
 	}
 }
